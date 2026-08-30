1b9d29cb89155fa5bec12a28833492586f215942
Rosk\Fernando Rosc <luiscasasola@unitec.edu>
2026-08-30 14:12:41 -0600

    Arreglo App.tsx (conectando con AppNavigator

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { LoginScreen } from './src/screens/LoginScreen';
+import { AppNavigator } from './src/navigation/AppNavigator';
 
 export default function App() {
-  // Mock simple de navegación para pruebas en Web
-  const mockNavigation: any = {
-    replace: (screenName: string) => alert(`Redirigiendo a: ${screenName}`),
-  };
-
-  return <LoginScreen navigation={mockNavigation} route={{} as any} />;
+  return <AppNavigator />;
 }
